2b5639b6de6faeecf0d024212ac02ec5e8c1e769
Deon Daigh <deon.daigh@gmail.com>
2023-03-22 15:07:24 -0500

    Initial Commit

---
+++ RaidBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new DiscordClient(new DiscordConfiguration()
+        {
+            Token = "MTA1NTM0NzI2NzM1NzM4NDg2NA.GpYcf5.Kx-BloPTQnXwXmKeRdWT2VlAYiwghZ6i4Aed2Y",
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.All
+        });
+
+        client.MessageCreated += async (s, e) =>
+        {
+            if (e.Message.Content.ToLower().StartsWith("ping"))
+                await e.Message.RespondAsync("pong!");
+        };
+
+        await client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+}
